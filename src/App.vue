15f3d5900ca0c5c4dde97e428179f136f44c6bf5
Huison <895672005@qq.com>
2024-04-25 00:37:55 +0800

    add chat

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
 
-import home from './views/home/index.vue'
 </script>
 
 <template>
-    <div class="full-screen-layout">
-        <router-view/>
-    </div>
+  <div class="full-screen-layout">
+    <router-view/>
+  </div>
 </template>
 
 <style scoped>
 .full-screen-layout {
-    width: 100%;
-    height: 100vh;
+  width: 100%;
+  height: 100vh;
 }
 </style>
--- src/views/nlp/chat/index.vue
+++ src/views/nlp/chat/index.vue
@@ -1,11 +1,52 @@
 <script setup lang="ts">
+import {ref} from 'vue'
 
+const count = ref(3)
+
+const add = () => {
+  count.value++
+}
+const onDelete = () => {
+  if (count.value > 0) {
+    count.value--
+  }
+}
 </script>
 
 <template>
-    对话
+  <el-container style="height: 100%;">
+    <el-aside style="border: 2px solid #9f9fbd; height: 100% ">
+            <el-button @click="add" class="chat-button">New chat</el-button>
+      <el-scrollbar>
+        <p v-for="item in count" :key="item" class="scrollbar-demo-item">
+          {{ item }}
+        </p>
+      </el-scrollbar>
+    </el-aside>
+    <el-main>聊天区域</el-main>
+
+  </el-container>
 </template>
 
 <style scoped>
+.scrollbar-demo-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  margin: 10px;
+  text-align: center;
+  border-radius: 4px;
+  background: var(--el-color-primary-light-9);
+  color: var(--el-color-primary);
+}
 
+.chat-button {
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  text-align: center;
+  width: 99%;
+  margin-top: 10px;
+}
 </style>
